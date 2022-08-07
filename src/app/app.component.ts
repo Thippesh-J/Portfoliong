9bd09efcb5cab325b6f92ecf90475abfd4e25e2e
Thippesha <jtthippeshaj@gmail.com>
2022-08-07 14:06:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'portfolio';
+
+  case: String | undefined;
+
+  ngOnInit(){
+    this.case = "About";
+  }
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
+  changeRightView(val:string){
+    this.case = val;
+  }
 }
